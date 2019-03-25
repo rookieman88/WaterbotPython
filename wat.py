edd93eca7e47747e0d05024e4d328fd1e318e2d1
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-03-25 20:40:33 +0900

    Update wat.py

--- wat.py
+++ wat.py
@@ -15,6 +15,22 @@ import youtube_dl
 from discord.ext import commands
 from discord.utils import find
 import requests as rq
+import random
+import time
+from urllib.request import urlopen, Request
+import urllib
+import bs4
+from urllib.request import Request
+import openpyxl
+import re
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import sys
+import json
+from selenium import webdriver
+
+
 set = setting.set()
 
 
@@ -23,6 +39,25 @@ app = discord.Client()
 thetoken = os.getenv("BOT_TOKEN")
 
 
+@app.event
+     if message.content.startswith('~날씨'):
+        try:
+            embed = discord.Embed(title='로딩중..',description='불러오고있습니다!',color=0x00ff00)
+            meg = await client.send_message(channel,embed=embed)
+            learn = message.content.split(" ")
+            location = learn[1]
+            enc_location = urllib.parse.quote(location+'날씨')
+            hdr = {'User-Agent': 'Mozilla/5.0'}
+            url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=' + enc_location
+            req = Request(url, headers=hdr)
+            html = urllib.request.urlopen(req)
+            bs_obj = bs4.BeautifulSoup(html, "html.parser")
+            div = bs_obj.find("span",{"class":'todaytemp'})
+            div2 = bs_obj.find("p",{"class":"cast_txt"})
+            embed = discord.Embed(title=location+'날씨',description=div.text+'℃'+'\n'+div2.text+'\n'+'네이버 날씨',color=0x00ff00)
+            await client.edit_message(meg,embed=embed)
+        except:
+           await client.send_message(channel,'없는 도시입니다!')
 
 
 
