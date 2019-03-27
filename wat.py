bf8c38002c03f7ba825c27e074641b075c6a63cd
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-03-27 19:38:51 +0900

    Update wat.py

--- wat.py
+++ wat.py
@@ -11,6 +11,7 @@ Copyright 헤브어 2019, All Right Reserved.
 """
 
 
+owner = ['417571990820618250']
 
 import discord
 import asyncio
@@ -28,8 +29,10 @@ import datetime
 import os
 import sys
 import json
+import setting
 from selenium import webdriver
 
+set = setting.set()
 client = discord.Client()
 times = int(time.time())
 afk = []
@@ -116,6 +119,105 @@ async def on_message(message):
                 colour=discord.Colour.green()
             )
             await client.send_message(message.channel,embed=embed)
+   
+    s = set.first + set.no
+    if "~공지" in message.content:
+        if message.author.id in owner:
+            notice = message.content.replace(s, "")
+            embed=discord.Embed(title="공지 시스템", color=0x80ff80)
+            embed.add_field(name="공지 발신 준비중!", value="<@" + message.author.id + ">", inline=True)
+            mssg = await app.send_message(message.channel, embed=embed)
+            a = []
+            b = []
+            e = []
+            ec = {}
+            embed=discord.Embed(title="공지 시스템", color=0x80ff80)
+            embed.add_field(name="공지 발신중!", value="<@" + message.author.id + ">", inline=True)
+            await app.edit_message(mssg, embed=embed)
+            for server in app.servers:
+                for channel in server.channels:
+                    for tag in set.allowprefix:
+                        if tag in channel.name:
+                            dtat = True
+                            for distag in set.disallowprefix:
+                                if distag in channel.name:
+                                    dtat = False
+                            if dtat:
+                                if not server.id in a:
+                                    try:
+                                        await app.send_message(channel, notice)
+                                    except discord.HTTPException:
+                                        e.append(str(channel.id))
+                                        ec[channel.id] = "HTTPException"
+                                    except discord.Forbidden:
+                                        e.append(str(channel.id))
+                                        ec[channel.id] = "Forbidden"
+                                    except discord.NotFound:
+                                        e.append(str(channel.id))
+                                        ec[channel.id] = "NotFound"
+                                    except discord.InvalidArgument:
+                                        e.append(str(channel.id))
+                                        ec[channel.id] = "InvalidArgument"
+                                    else:
+                                        a.append(str(server.id))
+                                        b.append(str(channel.id))
+            asdf = "```\n"
+            for server in app.servers:
+                if not server.id in a:
+                    if set.nfct:
+                        try:
+                            ch = await app.create_channel(server, set.nfctname)
+                            await app.send_message(ch, notice)
+                        except:
+                            asdf = asdf + str(server.name) + "[채널 생성 실패]\n"
+                        else:
+                            asdf = asdf + str(server.name) + "[채널 생성 및 재발송 성공]\n"
+                    else:
+                        asdf = asdf + str(server.name) + "\n"
+            asdf = asdf + "```"
+            embed=discord.Embed(title="공지 시스템", color=0x80ff80)
+            embed.add_field(name="공지 발신완료!", value="<@" + message.author.id + ">", inline=True)
+            bs = "```\n"
+            es = "```\n"
+            for bf in b:
+                bn = app.get_channel(bf).name
+                bs = bs + str(bn) + "\n"
+            for ef in e:
+                en = app.get_channel(ef).name
+                es = es + str(app.get_channel(ef).server.name) + "(#" + str(en) + ") : " + ec[ef] + "\n"
+            bs = bs + "```"
+            es = es + "```"
+            if bs == "``````":
+                bs = "``` ```"
+            if es == "``````":
+                es = "``` ```"
+            if asdf == "``````":
+                asdf = "``` ```"
+            sucess = bs
+            missing = es
+            notfound = asdf
+            embed.add_field(name="공지 발신 성공 채널:", value=sucess, inline=True)
+            embed.add_field(name="공지 발신 실패 채널:", value=missing, inline=True)
+            embed.add_field(name="공지 채널 없는 서버:", value=notfound, inline=True)
+            await app.edit_message(mssg, embed=embed)
+        else:
+           await app.send_message(message.channel, "봇 제작자만 사용할수 있는 커맨드입니다!")    
+
+
+
+from itertools import cycle
+        
+status = ['WaterBot v1.0', '주식기능 개발중!' , '꼬우면 oAsIcS#5074로 DMㄱㄱ', 'JS + Python 버전', '~도움 입력 가즈아ㅏㅏ']
+async def change_status():
+	await app.wait_until_ready()
+	msgelel = cycle(status)
+
+	while not app.is_closed:
+		current_status = next(msgelel)
+		await app.change_presence(game=discord.Game(name=current_status))
+		await asyncio.sleep(3)
+		
+app.loop.create_task(change_status())
 
 
 access_token = os.getenv('BOT_TOKEN')
