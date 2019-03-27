a3b52c53bd663e992d3a06607419ee030c73a9b7
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-03-27 20:07:58 +0900

    Update wat.py

--- wat.py
+++ wat.py
@@ -121,7 +121,7 @@ async def on_message(message):
             await client.send_message(message.channel,embed=embed)
 
         s = set.first + set.no
-        if message.content.startswith(s):
+        if message.content.startswith('~공지'):
             if message.author.id in owner:
                 notice = message.content.replace(s, "")
                 embed=discord.Embed(title="공지 시스템", color=0x80ff80)
