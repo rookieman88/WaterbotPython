919aa78d7034cd0d923ca7438018b708f63f52f2
Oasics <36586296+rookieman88@users.noreply.github.com>
2019-03-27 18:27:07 +0900

    Update wat.py

--- wat.py
+++ wat.py
@@ -1,19 +1,19 @@
+
+#WaterBot v.1 (Python Version) code
+
+#notice module by 메리
+#some command by haveadooday
+
+
 """
-WaterBot v.1 (Python Version) code
-
-notice module by 메리
-some command by haveadooday
+Github GNU General Public License version 3.0 (GPLv3)
+Copyright 헤브어 2019, All Right Reserved.
 """
 
-owner = ['417571990820618250']
 
+
+import discord
 import asyncio
-import discord 
-import setting
-import os
-from discord.ext import commands
-from discord.utils import find
-import requests as rq
 import random
 import time
 from urllib.request import urlopen, Request
@@ -25,27 +25,25 @@ import re
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import os
 import sys
 import json
 from selenium import webdriver
 
+client = discord.Client()
+times = int(time.time())
+afk = []
+warn = {}
+party = 'off'
+@client.event
+async def on_ready():
+     print('온라인(관리)')
+     print(client.user.name)
+     print('======')
 
-set = setting.set()
-
-
-app = discord.Client()
-
-thetoken = os.getenv("BOT_TOKEN")
-
-
-
-
-@app.event
-async def on_message(message):
-	
      if message.content.startswith('~날씨'):
         try:
-            meg = await app.send_message(channel,'로딩중... 1분정도 소요됩니다')
+            meg = await client.send_message(channel,'로딩중...')
             learn = message.content.split(" ")
             location = learn[1]
             enc_location = urllib.parse.quote(location+'날씨')
@@ -56,14 +54,70 @@ async def on_message(message):
             bs_obj = bs4.BeautifulSoup(html, "html.parser")
             div = bs_obj.find("span",{"class":'todaytemp'})
             div2 = bs_obj.find("p",{"class":"cast_txt"})
-            embed = discord.Embed(title=location+'날씨',description=div.text+'℃'+'\n'+div2.text+'\n'+'Powerd by 네이버 날씨',color=0x00ff00)
-            await app.send_message(meg,embed=embed)
+            embed = discord.Embed(title=location+'날씨',description=div.text+'℃'+'\n'+div2.text+'\n'+'네이버 날씨',color=0x00ff00)
+            await client.send_message(meg,embed=embed)
         except:
-          await app.send_message(channel,'없는 도시입니다!')
+           await client.send_message(channel,'없는 도시입니다!')
+            
+     if message.content.startswith('~미세먼지'):
+        try:
+           meg = await client.send_message(channel,'로딩중..')
+           learn = message.content.split(" ")
+           location = learn[1]
+           enc_location = urllib.parse.quote(location+'미세먼지')
+           hdr = {'User-Agent': 'Mozilla/5.0'}
+           url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=' + enc_location
+           req = Request(url, headers=hdr)
+           html = urllib.request.urlopen(req)
+           bs_obj = bs4.BeautifulSoup(html, "html.parser")
+           div = bs_obj.find("em",{"class":'main_figure'})
+           div2 = bs_obj.find("span",{'class':'update'})
+           learn2 = int(div.text)
+           if learn2 < 16:
+               a = '최고'
+           elif learn2 < 31:
+               a = '좋음'
+           elif learn2 < 41:
+               a = '양호'
+           elif learn2 < 51:
+               a = '보통'
+           elif learn2 < 76:
+               a = '나쁨'
+           elif learn2 < 101:
+               a = '상당히 나쁨'
+           elif learn2 < 151:
+               a = '매우나쁨'
+           elif learn2 > 150:
+               a = '최악'
+           embed = discord.Embed(title=learn[1]+'미세먼지',description='**'+str(learn2)+'㎍/㎥ \n'+a+'**\n'+div2.text+'\n'+'네이버 미세먼지/미세먼지 기준 미세미세8단계',color=0x00ff00)
+           await client.send_message(meg,embed=embed)
+        except:
+            await client.send_message(channel,'없는 도시 거나 도/시는 미세먼지 검색이 안됩니다.')
+
+     if message.content.startswith('~번역'):
+        learn = message.content.split(" ")
+        client_id = "cMk952QL7RsmQsctxHYP"
+        client_secret = "fvfG3a6Q_c"
+        encText = learn[1]
+        url = "https://openapi.naver.com/v1/papago/n2mt"
+
+        headers = {"X-Naver-Client-Id" :client_id,
+                "X-Naver-Client-Secret":client_secret}
+
+        params = (("source", "ko"),
+                ("target", "en"),
+                ("text", encText))
+
+        response = requests.post(url, data=params, headers=headers)
+        if response.status_code == 200:
+            response_body = response.json()
+            embed = discord.Embed(
+                title='한글->영어 번역결과',
+                description=response_body[u'message'][u'result'][u'translatedText'],
+                colour=discord.Colour.green()
+            )
+            await client.send_message(message.channel,embed=embed)
 
 
-
-
-
-			    
-app.run(thetoken)
+access_token = getenv('BOT_TOKEN')
+client.run(access_token)
